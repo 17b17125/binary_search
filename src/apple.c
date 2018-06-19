0ce9aea03335ee1eff09fdc902d076062e21cf01
Sasagawa Takuya <sasagawa.t.ae@is-mac24.edu.c.titech.ac.jp>
2018-06-19 15:28:54 +0900

    ub > lb nazo

--- src/apple.c
+++ src/apple.c
@@ -4,14 +4,41 @@ int n;
 int k;
 int A[100000];
 
+int p(int x){
+  int i = 0,j=0,l=0;
+
+  while(i < n){        //n人について話がすむまで繰り返します
+    while(x*j < A[i]){  //iさんはxこ入りの袋jこですみます
+      j++;
+    }
+    l+=j;               //iさん一人で使った袋の数
+    i++;                //次の人の話に移ります
+  }
+  if(l <= k){
+    return 1;           //xこ入りで十分
+  }else{
+    return 0;           //xこ入りでは袋が足りない
+  }
+}
 
 int main(){
   int i, lb, ub;
+
+  lb = 100001;
+  ub = 0;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  while(ub - lb > 1){
+    int m = (lb + ub) / 2;
+    if(p(m)){
+      ub = m;
+    }
+    else{
+      lb = m;
+    }
+  }
+  printf("%d\n", ub);
   return 0;
 }
