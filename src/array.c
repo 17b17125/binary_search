6c3dc6cbbbd47ad50e29b4d0335446d4701bfdc1
Sasagawa Takuya <sasagawa.t.ae@is-mac06.edu.c.titech.ac.jp>
2018-06-25 17:04:50 +0900

    関数 p の返り値の部分を修正、ubの初期値を変更

--- src/array.c
+++ src/array.c
@@ -5,8 +5,8 @@ int k;
 int A[100000];
 
 int p(int x){
-  if(A[x] >= k) return 1;   //A[x]>=k なので1を返す
-  return 0;                 //そうでない場合は0を返す
+  return A[x] >= k;   //A[x]>=k なので真(非0)を返す
+                      //そうでない場合は偽(0)を返す
 }
 
 int main(){
@@ -17,7 +17,7 @@ int main(){
     scanf("%d", &A[i]);
   }
   lb = 0;                   //ぜったいp(lb)=0になる初期値
-  ub = n-1;                 //ぜったいp(ub)=1になる初期値
+  ub = n;                 //ぜったいp(ub)=1になる初期値
   while(ub - lb > 1){
     int m = (lb + ub) / 2;
     if(p(m)){
